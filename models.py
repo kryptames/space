aa32b799b260b8b96613a26447f8728141e9ff9b
Nanthawan <nanthawan.ha@ku.th>
2017-09-28 18:09:57 +0700

    run slower

--- models.py
+++ models.py
@@ -1,13 +1,22 @@
 class Snake:
+    MOVE_WAIT = 0.2
+    
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
+        self.wait_time = 0
  
     def update(self, delta):
+        self.wait_time += delta
+ 
+        if self.wait_time < Snake.MOVE_WAIT:
+            return
+ 
         if self.x > self.world.width:
             self.x = 0
-        self.x += 5
+        self.x += 16
+        self.wait_time = 0
  
  
 class World:
